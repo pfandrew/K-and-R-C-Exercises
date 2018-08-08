805953310fb2f6a9545d4fbba902c5f6a57203da
gump <ahunt87654@gmail.com>
2018-08-07 22:36:23 -0500

    Update ex_1-11.c

--- ch1/ex_1-11.c
+++ ch1/ex_1-11.c
@@ -3,11 +3,12 @@
 /*
 	The tests would involve inputs with many of the characters which are tested for in the if/else loops:
 
-	"\n\n\t\tthe\tquick\n\nbrown\tfox\n jumped \tover \nthe\t  lazy\n     dog\t ."
+		"\n\n\t\tthe\tquick\n\nbrown\tfox\n jumped \tover \nthe\t  lazy\n     dog\t ."
 
 	Another idea is slightly incorrectly entered input:
 
 		"The quick brown fox jumped over the lazy dog ."
 
-	The inputs most likely to find bugs are those which are the longest and those containing many of the characters tested for, with very complicated arrangement. Another idea is slightly incorrectly typed input (punctuation with accidental spaces before it would trick the program into increasing the word count).
+	The inputs most likely to find bugs are those which are the longest and those containing many of the characters tested for, with very complicated arrangement. 
+	Another idea is slightly incorrectly typed input (punctuation with accidental spaces before it would trick the program into increasing the word count).
 */
