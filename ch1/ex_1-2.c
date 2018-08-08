610ede3053385febd968c6be6619a3fa5f634053
gump <ahunt87654@gmail.com>
2018-08-07 21:54:45 -0500

    Update ex_1-2.c

--- ch1/ex_1-2.c
+++ ch1/ex_1-2.c
@@ -1,7 +1,7 @@
 //compile: gcc ex_1-2.c
 //run: ./a.out
 
-//Exercise 1-2: Experiment to find out when printf's argument string contains \c, where c is some character not listed above.
+//Exercise 1-2: Experiment to find out what happens when printf's argument string contains \c, where c is some character not listed above.
 
 int main(){
 	printf("\\g: \g\n");
