f31c105f31c230e185402e6d4c6e565cca9804d5
gump <ahunt87654@gmail.com>
2018-08-12 18:08:41 -0500

    Update ex_1-24.c

--- ch1/ex_1-24.c
+++ ch1/ex_1-24.c
@@ -3,9 +3,9 @@
 
 //Exercise 1-24: Write a program to check a C program for rudimentary syntax errors like unbalanced parentheses, brackets and braces. Don't forget about quotes, both single and double, escape sequences, and comments. (This program is hard if you do it in full generality.)
 
-#include <stdio.h>
+//NOTE: This is still currently being debugged
 
-#define NUM_ES 	12
+#include <stdio.h>
 
 #define IN 1
 #define OUT 0
