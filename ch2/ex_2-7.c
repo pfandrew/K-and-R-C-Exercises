afd1c13c136dc81bdd790ac0b882ff4b805d94bd
gump <ahunt87654@gmail.com>
2018-08-18 02:13:21 -0500

    Update ex_2-7.c

--- ch2/ex_2-7.c
+++ ch2/ex_2-7.c
@@ -1,7 +1,7 @@
 //compile: gcc ex_2-7.c
 //run: ./a.out
 
-//Exercise 2-7: Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted (1.e, 1 changed into 0 and vice versa), leaving the others unchanged.
+//Exercise 2-7: Write a function invert(x,p,n) that returns x with the n bits that begin at position p inverted (i.e., 1 changed into 0 and vice versa), leaving the others unchanged.
 #include <stdio.h>
 void print_binary(unsigned int d){
 	if(d > 0){
