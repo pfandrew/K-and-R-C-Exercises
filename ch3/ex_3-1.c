e4884e8995df33cc4b879d3b1f1a3fb6f7763aed
gump <ahunt87654@gmail.com>
2018-08-19 17:18:51 -0500

    Update ex_3-1.c

--- ch3/ex_3-1.c
+++ ch3/ex_3-1.c
@@ -1,7 +1,9 @@
 //compile: gcc ex_3-1.c
 //run: time ./a.out
 
-//Exercise 3-1: Our binary search makes two tests inside the loop, when one would suffice (at the price of more tests outside). Write a version with only one test inside the loop and measure the difference in run-time.
+/*Exercise 3-1: Our binary search makes two tests inside the loop, when one would suffice (at the price of more tests outside).
+Write a version with only one test inside the loop and measure the difference in run-time.*/
+
 #include <stdio.h>
 
 int binsearch(int x, int v[], int n){
