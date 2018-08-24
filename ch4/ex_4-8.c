825b268ee80c08e1adb5d91b4af27c008483a9fa
gump <ahunt87654@gmail.com>
2018-08-23 23:19:52 -0500

    Add files via upload

---
+++ ch4/ex_4-8.c
@@ -0,0 +1,117 @@
+//compile: gcc ex_4-8.c
+//run: ./a.out
+
+//Exercise 4-8: Suppose that there will never be more than one character of pushback. Modify getch and ungetch accordingly.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define MAXOP 100
+#define NUMBER '0'
+#define MAXVAL 100
+#define BUFSIZE 100
+
+int getop(char []);
+void push(double);
+double pop(void);
+int getch(void);
+void ungetch(int);
+
+int sp = 0;
+double val[MAXVAL];
+char buf[BUFSIZE];
+int bufp = 0;
+
+int main(){
+	int type;
+	double op2;
+	char s[MAXOP];
+
+	while((type = getop(s)) != EOF){
+		switch(type){
+		case NUMBER:
+			push(atof(s));
+			break;
+		case '+':
+			push(pop() + pop());
+			break;
+		case '*':
+			push(pop() * pop());
+			break;
+		case '-':
+			op2 = pop();
+			push(pop() - op2);
+			break;
+		case '/':
+			op2 = pop();
+			if(op2 != 0.0)
+				push(pop() / op2);
+			else
+				printf("error: zero divisor\n");
+			break;
+		case '\n':
+			printf("\t%.8f\n", pop());
+			break;
+		default:
+			printf("error: unknown command %s\n", s);
+			break;
+		}
+	}
+	return 0;
+}
+
+void push(double f){
+	if(sp < MAXVAL)
+		val[sp++] = f;
+	else 
+		printf("error: stack full, can't push %f\n", f);
+}
+
+double pop(void){
+	if(sp > 0)
+		return val[--sp];
+	else{
+		printf("error: stack empty\n");
+		return 0.0;
+	}
+}
+
+int getop(char s[]){
+	int i, c;
+
+	while((s[0] = c = getch()) == ' ' || c == '\t')
+		;
+	s[1] = '\0';
+	if(!isdigit(c) && c != '.')
+		return c;
+	if(isdigit(c))
+		while(isdigit(s[++i] = c = getch()))
+			;
+	if(c == '.')
+		while(isdigit(s[++i] = c = getch()))
+			;
+	s[i] = '\0';
+	if(c != EOF)
+		ungetch(c);
+	return NUMBER;
+}
+
+int getch(void){
+	if(bufp == 1)
+	{
+		bufp = 0;
+		return buf[0];
+	}
+	else
+		return getchar();
+}
+
+void ungetch(int c){
+	if(bufp == 1)
+		printf("ungetch: too many characters\n");
+	else{
+		buf[0] = c;
+		bufp = 1;
+	}
+}
