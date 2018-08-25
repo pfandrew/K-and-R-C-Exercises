d15f3e627180d13decc6a9218682a55726807b76
gump <ahunt87654@gmail.com>
2018-08-25 04:37:00 -0500

    Add files via upload

---
+++ ch5/ex_5-1.c
@@ -0,0 +1,72 @@
+//compile: gcc ex_5-1.c
+//run: ./a.out
+
+//Exercise 5-1: As written, getint treats a + or - not followed by a digit as a valid representation of zero. Fix it to push such a character back on the input.
+
+#include <stdio.h>
+#include <ctype.h>
+
+#define MAXVAL 100
+#define BUFSIZE 100
+
+int getch(void);
+void ungetch(int);
+int get_int(int *pn);
+
+int sp = 0;
+double val[MAXVAL];
+char buf[BUFSIZE];
+int bufp = 0;
+
+
+int main(){
+	int c, k = 0;
+	int nums[MAXVAL];
+	while((c = get_int(&nums[k++])) != 'e')
+		k += c;
+	printf("You entered: ");
+	for(c = 0; c < k - 1; c++){
+		printf(" %d", nums[c]);
+	}
+	printf("\n");
+}
+
+int get_int(int *pn){
+	int c, d, sign;
+	while(isspace(c = getch()))
+		;
+	if(!isdigit(c) && c != EOF && c != '+' && c != '-'){
+		ungetch(c);
+		return -1;
+	}
+	sign = (c == '-') ? -1 : 1;
+	
+	if(c == '+' || c == '-'){
+		d = c;
+		c = getch();
+		if(!isdigit(c)){
+			ungetch(d); return -1;
+		}
+	}
+		
+	for(*pn = 0; isdigit(c); c = getch())
+		*pn = 10 * *pn + (c - '0');
+	*pn *= sign;
+	if(c != EOF)
+		ungetch(c);
+	else
+		return 'e';
+	return 0;
+}
+
+int getch(void){
+	return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c){
+	if(bufp >= BUFSIZE)
+		printf("ungetch: too many characters\n");
+	else
+		buf[bufp++] = c;
+}
+
