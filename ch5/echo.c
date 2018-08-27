73c09e3e6eadd572460ee15b4364381f5437c230
gump <ahunt87654@gmail.com>
2018-08-27 01:33:21 -0500

    Add files via upload

---
+++ ch5/echo.c
@@ -0,0 +1,10 @@
+//from section 5.10; version 1 of echo program
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	int i;
+	for(i = 1; i < argc; i++)
+		printf("%s%s", argv[i], (i < argc - 1) ? " ": "");
+	printf("\n");
+	return 0;
+}
