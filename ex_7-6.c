4b650b6866edea1fdaaa1d49b9d800535770413f
gump <ahunt87654@gmail.com>
2018-09-06 16:06:33 -0500

    Add files via upload

---
+++ ex_7-6.c
@@ -0,0 +1,46 @@
+//compile: gcc ex_7-6.c -o fcmp
+//run: ./fcmp [filename] [filename]
+
+#include <stdio.h>
+#define MAXLINE 10000
+int comparefiles(char *, char *);
+
+int main(int argc, char *argv[]){
+	if(argc != 3){
+		printf("usage: fcmp [filename] [filename]\n");
+		return 0;
+	}
+	
+	int r = comparefiles(argv[1], argv[2]);
+
+	return r;
+}
+
+int comparefiles(char *f1, char *f2){
+	FILE *fp1 = fopen(f1, "r");
+	FILE *fp2 = fopen(f2, "r");
+	int c, d, i = 0;
+	char line[MAXLINE];
+	while(((c = getc(fp1)) == (d = getc(fp2))) && c != EOF && d != EOF)	{
+		if(c == '\n'){
+			i = 0;
+			line[i] = '\0';
+		}
+		else{
+			line[i++] = c;
+		}
+	}
+
+	if(c != d){
+		while(c != EOF && c != '\n'){
+			line[i++] = c;
+			c = getc(fp1);
+		}
+	}
+
+	line[i] = '\0';
+	printf("%s\n", line);
+
+	fclose(fp1);
+	fclose(fp2);	
+}
