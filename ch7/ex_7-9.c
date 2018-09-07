dcbe9cce3053dc53e8c3552c2a8ff4ceeb99f96c
gump <ahunt87654@gmail.com>
2018-09-06 19:21:07 -0500

    Update ex_7-9.c

--- ch7/ex_7-9.c
+++ ch7/ex_7-9.c
@@ -3,11 +3,16 @@
 //run: time ./implem
 //run: time ./stdlib
 
+//Exercise 7-9: Functions like isupper can be implemented to save space or to save time. Explore both possibilities.
+
+//Note - this is the only chapter in the book with an odd number of exercises :(
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
 
-#define CHARS 1000000 /*at this size, the stdlib version reported a millisecond faster speed*/
+#define CHARS 1000000 /*at this size, the stdlib version of tolower() reported a millisecond faster speed*/
+
 int to_lower(int c){
 	if(c <= 90 && c >= 65)
 		c += 32;
